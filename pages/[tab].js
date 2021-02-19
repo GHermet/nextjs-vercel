f0f5fcae7cd7134f2f0ec7d9d0e47c910c74046a
guillaume <guillaume.hermet13@gmail.com>
2021-02-19 13:40:31 +0100

    updated [tab]

---
+++ pages/[tab].js
@@ -0,0 +1,37 @@
+import Head from 'next/head';
+import Link from 'next/link';
+import styles from '../styles/Home.module.css';
+
+export default function Tab({ tab }) {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>{tab}</title>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+
+      <main className={styles.main}>
+        <h1>About Page</h1>
+        <Link href='/'>
+          <a className={styles.card}>Home</a>
+        </Link>
+      </main>
+
+      <footer className={styles.footer}>
+        <a
+          href='https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          Powered by <img src='/vercel.svg' alt='Vercel Logo' className={styles.logo} />
+        </a>
+      </footer>
+    </div>
+  );
+}
+
+export const getServerSideProp = async ({ params }) => {
+  return {
+    props: { tab: String(params.tab) },
+  };
+};
